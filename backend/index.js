b281b83097325977fbbbfdee883ead3f8169164e
Iama01 <77541337+Iama01@users.noreply.github.com>
2023-08-06 06:37:35 +0300

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const { default: axios } = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+
+  try {
+    const r = await axios.put(
+      'https://api.chatengine.io/users/',
+      { username: username, secret: username, first_name: username },
+      { headers: { "Private-Key": "7cf09ce5-2469-465d-a657-e503c573195f" } }
+    )
+    return res.status(r.status).json(r.data)
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data)
+  }
+});
+
+app.listen(3001);
